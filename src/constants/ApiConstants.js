2f3ec84cc4505b5fffff32a74d5659177d69bfed
kaushik dholiya <kaushik.dholiya17@gmail.com>
2017-08-30 18:24:07 +0530

    Delete ApiConstants.js

--- src/constants/ApiConstants.js
+++
@@ -1,4 +0,0 @@
-const API = 'http://node-demo-wa.azurewebsites.net';
-export const getListingHeaders  = API + '/datatable/header';
-export const getTotalRecordsListingMaster = API + '/datatable/count';
-export const getListingRowData = API + '/listing/data';
